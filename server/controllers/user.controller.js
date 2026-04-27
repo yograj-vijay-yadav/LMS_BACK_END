7b7dd351f1e43164b9a74f43d361ec461cfbeddf
yograj-vijay-yadav <yograjyadav8888@gmail.com>
2026-04-27 20:06:17 +0530

    Updated coockieOptions issue for deployment

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,9 +9,10 @@ import User from '../models/user.model.js';
 import sendEmail from '../utils/sendEmail.js';
 
 const cookieOptions = {
-  secure: process.env.NODE_ENV === 'production' ? true : false,
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  secure: true,
   httpOnly: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 /*
